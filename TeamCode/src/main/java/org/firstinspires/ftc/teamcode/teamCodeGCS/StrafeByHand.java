27f4e7cc991eb8332c8897680decd1ce1dc8091f
CorbinYoung <corbin.young@eagles.oc.edu>
2017-12-23 12:31:59 -0600

    I've begun adding documentation, starting with the class Luke and I wrote

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamCodeGCS/StrafeByHand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamCodeGCS/StrafeByHand.java
@@ -2,26 +2,43 @@ package org.firstinspires.ftc.teamcode.teamCodeGCS;
 
 import org.firstinspires.ftc.teamcode.teamCode.HardwareNut;
 
+/**
+ * This class contains two small macro functions to handle strafing based on the trigger
+ *
+ * @author Luke Frazer
+ */
 public final class StrafeByHand {
 
-    private static final float value = 0.75f;
+    //We want to use 75% of the triggers value when setting power
+    private static final float percentage = 0.75f;
 
+    /**
+     * This method handles strafing left
+     *
+     * @param robot - reference to the actual robot
+     * @param trigger - value given by the controller
+     */
     public static void left(final HardwareNut robot, final double trigger) {
 
         //Sequence: rD -, lD +, rA +, lA -
-        robot.rightDrive.setPower(value * -trigger);
-        robot.leftDrive.setPower(value * trigger);
-        robot.rightArm.setPower(value * trigger);
-        robot.leftArm.setPower(value * -trigger);
-
+        robot.rightDrive.setPower(percentage * -trigger);
+        robot.leftDrive.setPower(percentage * trigger);
+        robot.rightArm.setPower(percentage * trigger);
+        robot.leftArm.setPower(percentage * -trigger);
     }
 
+    /**
+     * This method handles strafing right
+     *
+     * @param robot - reference to the actual robot
+     * @param trigger - value given by the controller
+     */
     public static void right(final HardwareNut robot, final double trigger) {
 
         //Sequence: rD +, lD -, rA -, lA +
-        robot.rightDrive.setPower(value * trigger);
-        robot.leftDrive.setPower(value * -trigger);
-        robot.rightArm.setPower(value * -trigger);
-        robot.leftArm.setPower(value * trigger);
+        robot.rightDrive.setPower(percentage * trigger);
+        robot.leftDrive.setPower(percentage * -trigger);
+        robot.rightArm.setPower(percentage * -trigger);
+        robot.leftArm.setPower(percentage * trigger);
     }
 }

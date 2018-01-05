e024730a452ceb3d8ba195600faebcb1a51e0eb0
Knights8081 <anna.field@gcsaz.org>
2018-01-05 16:40:05 -0700

    Corbin Young
    Guide update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamCode/HardwareNut.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamCode/HardwareNut.java
@@ -69,21 +69,21 @@ public class HardwareNut {
     /* Define global constants ------------------------------------------------------------------*/
 
     /* Arm Constants */
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
+    public static final double ARM_UP_POWER         =   0.45;
+    public static final double ARM_DOWN_POWER       =  -0.45;
 
     /* Servo Constants */
-    public static final double CLAW_MIN_RANGE  = 0.05;
-    public static final double CLAW_MAX_RANGE  = 1.3;
-    public static final double CLAW_HOME = 0.08;
-    public static final double CLAW_SPEED = 0.05;
-    public static final double MID_SERVO       =  0.0 ;
+    public static final double CLAW_MIN_RANGE       =   0.05;
+    public static final double CLAW_MAX_RANGE       =   1.3;
+    public static final double CLAW_HOME            =   0.08;
+    public static final double CLAW_SPEED           =   0.05;
+    public static final double MID_SERVO            =   0.0;
 
     /* Idol Hand Constants */
-    public static final double IDOLHAND_MIN_RANGE  = 0.05 ;
-    public static final double IDOLHAND_MAX_RANGE = 1.3 ;
-    public static final double IDOLHAND_HOME = 0.08;
-    public static final double IDOL_SPEED = 0.05;
+    public static final double IDOLHAND_MIN_RANGE   =   0.05;
+    public static final double IDOLHAND_MAX_RANGE   =   1.3;
+    public static final double IDOLHAND_HOME        =   0.08;
+    public static final double IDOL_SPEED           =   0.05;
 
 
     /**
